2474db3359f3df84771c1d1c35f572ae1c0b0c01
ashishchopra24 <49956840+ashishchopra24@users.noreply.github.com>
2019-08-15 18:58:11 +0530

    Qualifier Annotation - Fixed

--- src/main/java/com/example/dependency/controllers/ConstructorInjectedController.java
+++ src/main/java/com/example/dependency/controllers/ConstructorInjectedController.java
@@ -1,14 +1,18 @@
 package com.example.dependency.controllers;
 
 import com.example.dependency.services.GreetingService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
+//import com.example.dependency.services.ConstructorGreetingService;
 
 @Controller
 public class ConstructorInjectedController {
 
     private GreetingService greetingService;
 
-    public ConstructorInjectedController(GreetingService greetingService) {
+    @Autowired
+    public ConstructorInjectedController(@Qualifier("constructorGreetingService") GreetingService greetingService) {
         this.greetingService = greetingService;
     }
 
--- src/main/java/com/example/dependency/services/ConstructorGreetingService.java
+++ src/main/java/com/example/dependency/services/ConstructorGreetingService.java
@@ -1,5 +1,8 @@
 package com.example.dependency.services;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class ConstructorGreetingService implements GreetingService {
     @Override
     public String sayGreeting() {
