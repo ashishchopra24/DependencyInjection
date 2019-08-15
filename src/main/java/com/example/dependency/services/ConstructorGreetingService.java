f61e3a853ebf147ed3d3e05f414d480b8ee5c6fa
ashishchopra24 <49956840+ashishchopra24@users.noreply.github.com>
2019-08-15 18:01:14 +0530

    Qualifier Annotation - Error

---
+++ src/main/java/com/example/dependency/services/ConstructorGreetingService.java
@@ -0,0 +1,8 @@
+package com.example.dependency.services;
+
+public class ConstructorGreetingService implements GreetingService {
+    @Override
+    public String sayGreeting() {
+        return "Hello - I was injected via the constructor!!!";
+    }
+}
